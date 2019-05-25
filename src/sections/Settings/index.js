4c50f10808079ff48c331c202f48f14efad9d8b4
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-25 23:47:05 +0400

    add range in settings section

--- src/sections/Settings/index.js
+++ src/sections/Settings/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-import FormGroup from '@material-ui/core/FormGroup';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+
+import Range from './components/Range';
+
+import Wrapper from './components/Wrapper';
 
 import { useStore } from 'store';
 
@@ -13,6 +18,7 @@ const Settings = _ => {
       primeMinBoundaries: { show: showPrimeMinBoundaries },
       primeMaxBoundaries: { show: showPrimeMaxBoundaries },
       custom: { show: showCustom },
+      range,
     },
 
     actions: {
@@ -20,50 +26,60 @@ const Settings = _ => {
       switchPrimeMinBoundariesView,
       switchPrimeMaxBoundariesView,
       switchCustomView,
+      setRange,
     },
   } = useStore();
 
-  return <FormGroup>
-    <FormControlLabel
-      control={
-        <Switch
-          checked={showPrimeNumbers}
-          onChange={_ => switchPrimeNumbersView(!showPrimeNumbers)}
-        />
-      }
-      label="Prime Numbers"
-    />
-    <FormControlLabel
-      control={
-        <Switch
-          checked={showPrimeMinBoundaries}
-          onChange={_ => switchPrimeMinBoundariesView(!showPrimeMinBoundaries)}
-          color="primary"
-        />
-      }
-      label="Prime Min Boundaries"
-    />
-    <FormControlLabel
-      control={
-        <Switch
-          checked={showPrimeMaxBoundaries}
-          onChange={_ => switchPrimeMaxBoundariesView(!showPrimeMaxBoundaries)}
-          color="primary"
-        />
-      }
-      label="Prime Max Boundaries"
-    />
-    <FormControlLabel
-      control={
-        <Switch
-          checked={showCustom}
-          onChange={_ => switchCustomView(!showCustom)}
-          color="primary"
-        />
-      }
-      label="Custom"
-    />
-  </FormGroup>;
+  function handleRangeChange(value) {
+    setRange(value);
+  }
+
+  return <Wrapper>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Switch prime generators</FormLabel>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={showPrimeNumbers}
+            onChange={_ => switchPrimeNumbersView(!showPrimeNumbers)}
+          />
+        }
+        label="Prime Numbers"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={showPrimeMinBoundaries}
+            onChange={_ => switchPrimeMinBoundariesView(!showPrimeMinBoundaries)}
+            color="primary"
+          />
+        }
+        label="Prime Min Boundaries -> n⋅ln(n) + n⋅(ln(ln(n)) − 1)"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={showPrimeMaxBoundaries}
+            onChange={_ => switchPrimeMaxBoundariesView(!showPrimeMaxBoundaries)}
+            color="primary"
+          />
+        }
+        label="Prime Max Boundaries -> n⋅ln(n) + n⋅ln(ln(n)"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={showCustom}
+            onChange={_ => switchCustomView(!showCustom)}
+            color="primary"
+          />
+        }
+        label="Custom"
+      />
+      <FormLabel component="legend">Choose range</FormLabel>
+      <Range defaultValue={range.to} onChange={handleRangeChange} />
+    </FormControl>
+  </Wrapper>
 };
 
 export default Settings;
