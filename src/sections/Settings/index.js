54263f32e745d02d3129354d1b9ff447a6228f43
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-25 17:23:21 +0400

    add switch buttons to settings page

--- src/sections/Settings/index.js
+++ src/sections/Settings/index.js
@@ -1,7 +1,69 @@
 import React from 'react';
 
-const Settings = _ => (
-  <h4>Settings</h4>
-);
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+import { useStore } from 'store';
+
+const Settings = _ => {
+  const {
+    state: {
+      primeNumbers: { show: showPrimeNumbers },
+      primeMinBoundaries: { show: showPrimeMinBoundaries },
+      primeMaxBoundaries: { show: showPrimeMaxBoundaries },
+      custom: { show: showCustom },
+    },
+
+    actions: {
+      switchPrimeNumbersView,
+      switchPrimeMinBoundariesView,
+      switchPrimeMaxBoundariesView,
+      switchCustomView,
+    },
+  } = useStore();
+
+  return <FormGroup>
+    <FormControlLabel
+      control={
+        <Switch
+          checked={showPrimeNumbers}
+          onChange={_ => switchPrimeNumbersView(!showPrimeNumbers)}
+        />
+      }
+      label="Prime Numbers"
+    />
+    <FormControlLabel
+      control={
+        <Switch
+          checked={showPrimeMinBoundaries}
+          onChange={_ => switchPrimeMinBoundariesView(!showPrimeMinBoundaries)}
+          color="primary"
+        />
+      }
+      label="Prime Min Boundaries"
+    />
+    <FormControlLabel
+      control={
+        <Switch
+          checked={showPrimeMaxBoundaries}
+          onChange={_ => switchPrimeMaxBoundariesView(!showPrimeMaxBoundaries)}
+          color="primary"
+        />
+      }
+      label="Prime Max Boundaries"
+    />
+    <FormControlLabel
+      control={
+        <Switch
+          checked={showCustom}
+          onChange={_ => switchCustomView(!showCustom)}
+          color="primary"
+        />
+      }
+      label="Custom"
+    />
+  </FormGroup>;
+};
 
 export default Settings;
