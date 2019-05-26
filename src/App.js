7ba2e7bab869193bb8824b952dc086e410166902
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-26 12:41:01 +0400

    clearify App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import Layout from 'layout';
 
-import { useStore } from 'store';
-import { useMount } from 'hooks';
-
-function App(props) {
-
-  const { actions: {
-    generatePrimeNumbers,
-    generatePrimeMinBoundaries,
-    generatePrimeMaxBoundaries,
-    generateCustom,
-  }} = useStore();
-
-  useMount(_ => {
-    generatePrimeNumbers();
-    generatePrimeMinBoundaries();
-    generatePrimeMaxBoundaries();
-    generateCustom();
-  });
-
-  return (
-    <Layout />
-  );
-}
+const App = props => <Layout />;
 
 export default App;
