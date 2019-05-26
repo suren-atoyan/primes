4c1ee4b3a277c7b31ba3f687206e3433f3fec625
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-26 12:44:31 +0400

    remove all imperative methods from store/actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,22 @@
 import { createOvermind } from 'overmind';
 import { createHook } from 'overmind-react';
 
-import { getPrime, primeBoundaries } from 'utils';
-
 export const store = createOvermind({
   state: {
-    layout: {
-      isSideBarOpen: false,
-    },
-
     primeNumbers: {
       show: true,
-      list: [],
     },
 
     primeMinBoundaries: {
       show: false,
-      list: [],
     },
 
     primeMaxBoundaries: {
       show: false,
-      list: [],
     },
 
     custom: {
       show: false,
-      list: [],
       formula: `n => Math.ceil(2**(1.5 * Math.log2(n)) + Math.log2(n) + 1 - 2**Math.log2(n))`,
     },
 
@@ -37,46 +27,6 @@ export const store = createOvermind({
   },
 
   actions: {
-    toggleSideBarOpen({ state }) {
-      state.layout.isSideBarOpen = !state.layout.isSideBarOpen;
-    },
-
-    generatePrimeNumbers({ state: { primeNumbers, range: { from, to } } }) {
-      // For now we will get prime numbers belonged mentioned interval
-      // from predefined list of prime numbers (up to 1000th)
-      // In next development cycle we will design and develop
-      // the most proper way to do it programaticly
-
-      // TODO: here we can do optimizations to avoid
-      // re-generating of existing numbers, which
-      // can be just overwrited by the same value
-      primeNumbers.list = Array(to - from + 1).fill().map((_, i) => getPrime(i + 1));
-    },
-
-    generatePrimeMinBoundaries({ state: { primeMinBoundaries, range: { from, to } } }) {
-      // TODO: the structure of this code segment is repeating;
-      // can be made a generic function for those methods
-      primeMinBoundaries.list = Array(to - from + 1).fill().map((_, i) => primeBoundaries.min(i + 1));;
-    },
-
-    generatePrimeMaxBoundaries({ state: { primeMaxBoundaries, range: { from, to } } }) {
-      // TODO: the structure of this code segment is repeating;
-      // can be made a generic function for those methods
-      primeMaxBoundaries.list = Array(to - from + 1).fill().map((_, i) => primeBoundaries.max(i + 1));;
-    },
-
-    generateCustom({ state: { custom, range: { from, to } } }) {
-      // TODO: the structure of this code segment is repeating;
-      // can be made a generic function for those methods
-
-      // NOTE: it should be implemented in next
-      // development stage. Now it just for demo.
-      // const formula = primeBoundaries.min;
-      const formula = primeBoundaries.min;
-
-      custom.list = Array(to - from + 1).fill().map((_, i) => formula(i + 1));
-    },
-
     switchPrimeNumbersView({ state: { primeNumbers } }, show) {
       primeNumbers.show = show;
     },
@@ -97,20 +47,10 @@ export const store = createOvermind({
       custom.formula = formula;
     },
 
-    setRange({ state: { range }, actions: {
-      generatePrimeNumbers,
-      generatePrimeMinBoundaries,
-      generatePrimeMaxBoundaries,
-      generateCustom,
-    }}, to, from = 1) {
+    setRange({ state: { range }}, to, from = 1) {
       if (to !== range.to || from !== range.from) {
         range.from = from;
         range.to = to;
-
-        generatePrimeNumbers();
-        generatePrimeMinBoundaries();
-        generatePrimeMaxBoundaries();
-        generateCustom();
       }
     }
   },
