378d63f1509fb443d0e1174276ff41caeb68ad38
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-27 23:56:43 +0400

    fix nivo chart exeption related to data with less than 3 length

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ResponsiveLine } from '@nivo/line'
+import { ResponsiveLine } from '@nivo/line';
 
 import config from 'config';
 
@@ -10,6 +10,9 @@ const LineChart = ({ data }) => {
   return <ResponsiveLine
     data={data}
     {...config.graphics.lineChartProps}
+    useMesh={data.to - data.from + 1 > 2} // those very suspicious magic numbers
+    // have appeared just because in those cases when we have data to represent
+    // with less than 3 lengths, "nivo" charts don't work with mesh.
   />;
 };
 
