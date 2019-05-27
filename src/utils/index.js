bc1a5752226cacdcf0b7bf2ed6d9336f8cbf0c87
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-27 18:18:10 +0400

    --amend

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,14 +6,18 @@ import saveInUrl from './saveInUrl';
 import str2hex from './str2hex';
 import hex2str from './hex2str';
 import parseState from './parseState';
+import resetApp from './resetApp';
+import ensureLastLine from './ensureLastLine';
 import noop from './noop';
 import * as primeBoundaries from './primeBoundaries';
 
 export {
   getPrime,
   primeBoundaries,
+  ensureLastLine,
   transformToChartData,
   monacoEditor,
+  resetApp,
   parseState,
   calcPrimes,
   saveInUrl,
