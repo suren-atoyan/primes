136f374cf9931adc056099efc1e8d291531461b3
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-28 14:03:38 +0400

    check data existence before chart render

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -3,12 +3,14 @@ import { ResponsiveLine } from '@nivo/line';
 
 import config from 'config';
 
-const LineChart = ({ data: { from, to, list } }) => <ResponsiveLine
-  data={list}
-  {...config.graphics.lineChartProps}
-  useMesh={to - from + 1 > 3} // those very suspicious magic numbers
-  // have appeared just because in those cases when we have data to represent
-  // with less than 3 lengths, "nivo" charts don't work with mesh.
-/>;
+const LineChart = ({ data: { from, to, list } }) => list.length
+  ? <ResponsiveLine
+      data={list}
+      {...config.graphics.lineChartProps}
+      useMesh={to - from + 1 > 3} // those very suspicious magic numbers
+      // have appeared just because in those cases when we have data to represent
+      // with less than 3 lengths, "nivo" charts don't work with mesh.
+    />
+  : null;
 
 export default LineChart;
