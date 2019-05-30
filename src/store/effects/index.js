10a7b474c038f3d75ac4f75697f0422edaedbb7e
Suren Atoyan <surenatoyan93@gmail.com>
2019-05-30 17:15:16 +0400

    add sequence gernerator for lower and upper bounds too

--- src/store/effects/index.js
+++ src/store/effects/index.js
@@ -1,7 +1,7 @@
 import { runner } from 'utils';
 
 const calculate = {
-  async cutomSequence(formula, { from ,to }, fnName) {
+  async genSequence(formula, { from ,to }, fnName) {
     const source = `
       ${formula};
       const result = Array(${to} - ${from} + 1).fill().map((_, i) => ${fnName}(i + 1));
@@ -9,7 +9,24 @@ const calculate = {
     `;
 
     return await runner(source, '__x__');
-  }
+  },
+
+  customSequence(formula, { from ,to }, fnName) {
+    return new Promise(async (res, rej) => {
+      const result = await calculate.genSequence(formula, { from ,to }, fnName);
+
+      if (result.error || result.timeout) return rej(result);
+      return res(result);
+    });
+  },
+
+  primeLowerBound(formula, { from ,to }, fnName) {
+    return calculate.genSequence(formula, { from ,to }, fnName);
+  },
+
+  primeUpperBound(formula, { from ,to }, fnName) {
+    return calculate.genSequence(formula, { from ,to }, fnName);
+  },
 };
 
 export { calculate };
