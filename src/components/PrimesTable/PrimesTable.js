16fa377023840e0b14683e87373ac866376ac910
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-11 21:24:05 +0400

    remove Paper wrapper

--- src/components/PrimesTable/PrimesTable.js
+++ src/components/PrimesTable/PrimesTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 
@@ -17,30 +16,28 @@ const PrimesTable = ({ data: {
   from,
   to,
 }}) => (
-  <Paper elevation={0} className="full-width">
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>nth</TableCell>
-          {primeLowerBound && <TableCell align="right">n⋅ln(n) + n⋅(ln(ln(n)) − 1)</TableCell>}
-          {primeNumbers && <TableCell data-prime align="right">nth prime number</TableCell>}
-          {primeUpperBound && <TableCell align="right">n⋅ln(n) + n⋅ln(ln(n)</TableCell>}
-          {customSequence && <TableCell align="right">Custom</TableCell>}
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell>nth</TableCell>
+        {primeLowerBound && <TableCell align="right">n⋅ln(n) + n⋅(ln(ln(n)) − 1)</TableCell>}
+        {primeNumbers && <TableCell data-prime align="right">nth prime number</TableCell>}
+        {primeUpperBound && <TableCell align="right">n⋅ln(n) + n⋅ln(ln(n)</TableCell>}
+        {customSequence && <TableCell align="right">Custom</TableCell>}
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {Array(to - from + 1).fill().map((_, i) => (
+        <TableRow key={i}>
+          <TableCell component="th" scope="row">{i + 1}</TableCell>
+          {primeLowerBound && <TableCell align="right">{primeLowerBound[i] || '-'}</TableCell>}
+          {primeNumbers && <TableCell data-prime align="right">{primeNumbers[i]}</TableCell>}
+          {primeUpperBound && <TableCell align="right">{primeUpperBound[i] || '-'}</TableCell>}
+          {customSequence && <TableCell align="right">{customSequence[i] || '-'}</TableCell>}
         </TableRow>
-      </TableHead>
-      <TableBody>
-        {Array(to - from + 1).fill().map((_, i) => (
-          <TableRow key={i}>
-            <TableCell component="th" scope="row">{i + 1}</TableCell>
-            {primeLowerBound && <TableCell align="right">{primeLowerBound[i] || '-'}</TableCell>}
-            {primeNumbers && <TableCell data-prime align="right">{primeNumbers[i]}</TableCell>}
-            {primeUpperBound && <TableCell align="right">{primeUpperBound[i] || '-'}</TableCell>}
-            {customSequence && <TableCell align="right">{customSequence[i] || '-'}</TableCell>}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </Paper>
+      ))}
+    </TableBody>
+  </Table>
 );
 
 PrimesTable.propTypes = {
