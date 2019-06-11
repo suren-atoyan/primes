3967b70b7a045155c93e74d9045266ba35086a9a
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-11 21:26:02 +0400

    use ThemeProvider over Layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -6,6 +6,8 @@ import Formulas from './Formulas';
 import { useSplitterClick } from 'utils/hooks';
 import getLayoutSplitterProps from './getLayoutSplitterProps';
 
+import { ThemeProvider } from 'theme';
+
 import SplitterLayout from 'react-splitter-layout';
 import 'react-splitter-layout/lib/index.css';
 
@@ -13,18 +15,20 @@ const Layout = _ => {
   const [isSettingsSectionOpen, setIsSettingsSectionOpen] = useState(false);
   useSplitterClick(_ => setIsSettingsSectionOpen(!isSettingsSectionOpen));
 
-  return <SplitterLayout
-    {...getLayoutSplitterProps(isSettingsSectionOpen)}
-    // strange? not exactly
-    // after "DragEnd" of splitter, we have to dispatch a resize event
-    // to the window to avoid non-actual layout of elements
-    // inside that particular pane (for example
-    // "tab indicators" fo Material-UI)
-    onDragEnd={_ => window.dispatchEvent(new Event('resize'))}
-  >
-    <DataVisualization />
-    <Formulas />
-  </SplitterLayout>
+  return <ThemeProvider>
+    <SplitterLayout
+      {...getLayoutSplitterProps(isSettingsSectionOpen)}
+      // strange? not exactly
+      // after "DragEnd" of splitter, we have to dispatch a resize event
+      // to the window to avoid non-actual layout of elements
+      // inside that particular pane (for example
+      // "tab indicators" fo Material-UI)
+      onDragEnd={_ => window.dispatchEvent(new Event('resize'))}
+    >
+      <DataVisualization />
+      <Formulas />
+    </SplitterLayout>
+  </ThemeProvider>
 };
 
 export default Layout;
