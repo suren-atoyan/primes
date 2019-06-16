43c02be04d075d0bd8302a7b69789b00906186b1
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-16 12:02:57 +0400

    fix tabulation in config file

--- src/config/index.js
+++ src/config/index.js
@@ -87,15 +87,15 @@ const config = {
     value: '',
 
     theme: {
-    'night-dark': {
-      base: 'vs-dark',
-      inherit: true,
-      rules: [],
-      colors: {
-        'editor.background': '#202124',
+      'night-dark': {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [],
+        colors: {
+          'editor.background': '#202124',
+        },
       },
     },
-  },
   },
 
   defaultFnName,
